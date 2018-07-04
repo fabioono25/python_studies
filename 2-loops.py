6b90397d0fdf4e9501ad472bdb6d5186f8f3882e
Fabio Tavoloni <fabio.t0844.beyond@xpi.com.br>
2018-07-04 08:31:59 -0300

    working with loops

--- 2-loops.py
+++ 2-loops.py
@@ -36,12 +36,39 @@ def print_blockchain_elements():
     for block in blockchain:
         print('Outputting Block')
         print(block)
+    else:
+        print('-' * 20)
+
+def verify_chain():
+    # block_index = 0
+    is_valid = True
+    # for block in blockchain:
+    #     if block_index == 0:
+    #         block_index += 1
+    #         continue
+    #     if block[0] == blockchain[block_index-1]:
+    #         is_valid = True
+    #     else:
+    #         is_valid = False
+    #         break
+    #     block_index += 1
+    for block_index in range(len(blockchain)):
+        if block_index == 0:
+            continue
+        if blockchain[block_index] == blockchain[block_index-1]:
+            is_valid = True
+        else:
+            is_valid = False
+    return is_valid
+
+waiting_for_input = True
 
 # while loop
-while True:
+while waiting_for_input:
     print("please choose")
     print('1: add a new transaction value')
     print('2: output the blockchain blocks ')
+    print('h: manipulate the chain')
     print('q: Quit ')
     user_choice = get_user_choice()
 
@@ -50,10 +77,19 @@ while True:
         add_transaction(tx_amount, get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_elements()
+    elif user_choice == 'h':
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
     elif user_choice == 'q':
-        break
+        waiting_for_input = False
     else:
         print('input was invalid, please pick a value from the list!')
-    print('choice registered!')
+    if not verify_chain():
+        print_blockchain_elements()
+        print('invalid blockchain!')
+        break
+else:
+    print('user left')
+
 
 print('Done!')
